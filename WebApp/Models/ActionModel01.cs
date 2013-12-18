06eadc0c52c7187c629933838e41fa4fccc08662
guillaume-delattre <guillaume.delattre@hotmail.fr>
2013-12-18 23:40:02 +0700

    Test a webapp ASP MVC 5 Webapp

---
+++ WebApp/Models/ActionModel01.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class ActionModel01
+    {
+        [Required]
+        [Range(1, 200)]
+        public double? Weight { get; set; }
+
+        [Required]
+        [Range(1, 150)]
+        public int? Age { get; set; }
+    }
+}
