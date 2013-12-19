1c93c70372b8b5b46bed1bf610ff62163034b1f0
guillaume-delattre <guillaume.delattre@hotmail.fr>
2013-12-19 18:39:25 +0700

    Building a ASP.NET MVC Application

--- MVC/Controllers/FirstController.cs
+++ MVC/Controllers/FirstController.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class FirstController : Controller
-    {        public ActionResult Index()
+    {        
+        public ActionResult Index()
         {
             return View();
+        }
+
+        public ViewResult Action01()
+        {
+            ViewBag.info = string.Format("Contrôleur={0}, Action={1}", RouteData.Values["controller"], RouteData.Values["action"]);
+            return View();
+        }
+
+        public ViewResult Action02()
+        {
+            return View(new ViewModel());
         }
 	}
 }
---
+++ MVC/Models/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace MVC.Models
+{
+    public class ViewModel
+    {
+        public Personne[] Personnes { get; set; }
+        public int SelectedId { get; set; }
+
+        public ViewModel()
+        {
+            Personnes = new Personne[] { 
+            new Personne{ Id = 1, Prenom = "Pierre", Nom = "Martino"}, 
+            new Personne{ Id = 2, Prenom = "Pauline", Nom = "Pereiro"}, 
+            new Personne{ Id = 3, Prenom = "Jacques", Nom = "Alfonso"} };
+            SelectedId = 2;
+        }
+    }
+
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+    }
+}
